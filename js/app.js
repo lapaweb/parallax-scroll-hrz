d781e97bd309764ec7bade96219e2d56cb665f5c
lapaweb <lapa-34@ya.ru>
2023-02-04 22:30:43 +0300

    свободный скролл

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 new Swiper('.slider', {
-  speed: 2000,
-  mousewheel: true,
+  speed: 2400,
+  mousewheel: {
+    enabled: true,
+    sensitivity: 2.4
+  },
   spaceBetween: 18,
-  parallax: true
+  parallax: true,
+  freeMode: true
 })
